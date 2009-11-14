1f7158e2bc7c6e627907caa7c1d16139c4ba8989
winston <dyls@alioth.net>
2009-11-14 16:33:55 +0000

    First pass of 128K snapshotting

--- modules/basext/savesna.asm
+++ modules/basext/savesna.asm
@@ -26,6 +26,10 @@
 ; F_savesna48
 ; Save a 48K snapshot. Null terminated filename pointed to by HL.
 UNPAGE	equ	0x007C
+SAVERAM	equ	0x3600
+MAINSPSAVE equ	0x35FE
+TEMPSP	equ	0x81FE
+
 F_snaptest
 	ld hl, STR_saving
 	call PRINT42
@@ -40,7 +44,7 @@ F_snaptest
 .done
 	ld (de), a
 	ld hl, 0x3000
-	call F_savesna48
+	call F_savesna
 	jr c, .borked
 
 	ld a, (SNA_EIDI)		; Re.enable interrupts?
@@ -76,10 +80,11 @@ STR_saving defb "Saving snapshot.sna...\n",0
 STR_filename defb "snapshot.sna",0
 
 
-;
+;------------------------------------------------------------------------
+; HL = pointer to filename string
 ; Entry to the NMI saves the folowing at NMISTACK-4:
 ; hl, de, bc, af, af'
-F_savesna48
+F_savesna
 	ld d, O_CREAT | O_TRUNC		; Flags
 	ld e, O_WRONLY			; File mode
 	call OPEN			; Open the snapshot file
@@ -111,18 +116,16 @@ F_savesna48
 	ld (SNA_DEALT), de
 	ld (SNA_BCALT), bc
 	exx
-	ld a, i
-	ld (SNA_I), a
+	ld a, i				; also sets P/V flag if IFF2 is
+	ld (SNA_I), a			; set (interrupts enabled)
+	call pe, .setei
 
 	ld bc, CTRLREG			; read the border colour
 	in a, (c)			; from the CPLD.
 	and 0x07			; mask out all the high bits.
 	ld (SNA_BORDER), a
 
-	; Now something must be done to detect whether interrups were
-	; enabled, and what interrupt mode we're in. Only IM1 and IM2
-	; can really be used.
-	; Make an IM2 vector table.
+	; Now something must be done to detect interrupt mode
 	ld hl, 0x3600
 	ld de, 0x3601
 	ld bc, 258			; table is actually 0x100 bytes
@@ -137,46 +140,20 @@ F_savesna48
 	push hl				; on the stack so RETN
 	retn				; takes us just to the next addr.
 
-.continue	
-	ld a, 4				; posit that interrupts are
-	ld (SNA_EIDI),a			; enabled.
-	ld hl, 3000			; Waste enough T-states so
-.loop	dec hl				; that at least one interrupt
-	ld a, h				; will occur. (26 T-states per
-	or l				; iteration)
-	jr nz, .loop
-
-	ld hl, (v_intcount)		; did the counter increase?
-	ld a, h
-	or l
-	jr z, .notim1			; No interrupt occurred in IM 1
-
-	ld a, 1
-	ld (SNA_IM), a
-	jr .donewithinterrupts
-
-.notim1
-	ld a, (SNA_IM)			; did the IM 2 routine record
-	cp 2				; IM 2?
-	jr z, .donewithinterrupts
-
-	; Now we have to try it all over again after explicitly enabling
-	; interrupts.
-	xor a
-	ld (SNA_EIDI), a		; interrupts weren't enabled
+.continue
 	ei
 	ld hl, 3000			; Waste enough T-states so
 .loop2	dec hl				; that at least one interrupt
-	ld a, h				; will occur.
-	or l
-	jr nz, .loop2
+	ld a, h				; will occur. The IM 2 routine will
+	or l				; set "this is IM 2" itself, if
+	jr nz, .loop2			; not we'll need to set it.
 
 	ld hl, (v_intcount)		; did intcount update?
 	ld a, h
 	or l
 	jr z, .donewithinterrupts	; Done.
 
-	ld a, 1
+	ld a, 1				; Not IM 2 if intcount increased,
 	ld (SNA_IM), a			; set interrupt mode 1
 
 	; Now it's all over bar the shouting.
@@ -187,13 +164,92 @@ F_savesna48
 	ld a, (SNA_I)
 	ld i, a
 
+	; Detect whether we're saving a 48K or a 128K snapshot
+	call F_detectpages		; if carry sets, not 128K
+	jr nc, .save128snap
+
+	call .savemain			; save header and memory
+.writedone
+	ld a, (v_snapfd)		; close the file
+	call VCLOSE
+	ret
+
+.writeerr
+	push af
+	ld a, (v_snapfd)
+	call VCLOSE
+	pop af
+	ret
+.setei
+	ld a, 4
+	ld (SNA_EIDI), a
+	ret
+
+.save128snap
+	; adjust header suitably for 128K snapshots
+	ld hl, (SNA_SP)			; The stack pointer must be
+	ld e, (hl)			; adjusted and the PC must be
+	inc hl				; obtained.
+	ld d, (hl)			; PC now in DE
+	inc hl
+	ld (SNA_SP), hl			; Set the snapshot SP.
+	ld (SNA_PC), de			; Set the PC
+	xor a
+	ld (SNA_TRDOS), a		; TRDOS always 0
+	call .savemain			; the header + 48K
+	jr c, .writeerr
+
+	ld hl, SNA_PC			; Save the 128K second header
+	ld bc, 4			; (4 bytes long)
+	ld a, (v_snapfd)
+	call WRITE
+	jr c, .writeerr
+
+	; Save the 128K pages at 0xC000.
+	ld a, (SNA_7FFD)		; Skip the page we've aready
+	and 7				; saved (as it was in 0xC000)
+	ld e, a
+	xor a				; start from page 0
+.save128loop
+	cp e				; skip the page that was already
+	jr z, .next			; saved, also skip pages 2 and 5
+	cp 0x02
+	jr z, .next
+	cp 0x05
+	jr z, .next
+	ld bc, 0x7FFD			; set the page we want to save
+	out (c), a
+
+	push af
+	push de
+	ld hl, 0xC000
+	ld bc, 0x4000
+	call F_saveblock
+	jr c, .writeerr128
+	pop de
+	pop af
+.next
+	inc a
+	cp 0x08				; all pages have been saved
+	jr nz, .save128loop
+	ld a, (SNA_7FFD)		; restore 128K paging
+	ld bc, 0x7FFD
+	out (c), a
+	jr .writedone
+
+.writeerr128
+	pop de
+	pop de				; don't overwrite AF
+	jr .writeerr
+
+.savemain
 	; TODO: Something ought to be done with the R register, really.
 	; Save the interrupts-and-stuff block.
 	ld hl, HEADER
 	ld bc, HEADERSZ
 	ld a, (v_snapfd)
 	call WRITE
-	jr c, .writeerr
+	ret c
 
 	; Save memory. First, restore screen memory so that it can be
 	; saved to the file.
@@ -213,18 +269,6 @@ F_savesna48
 	ld hl, 16384			; Start saving from this address
 	ld bc, 49152			; For this many bytes
 	call F_saveblock
-	jr c, .writeerr
-
-.writedone
-	ld a, (v_snapfd)		; close the file
-	call VCLOSE
-	ret
-
-.writeerr
-	push af
-	ld a, (v_snapfd)
-	call VCLOSE
-	pop af
 	ret
 
 ;-----------------------------------------------------------------------
@@ -251,4 +295,153 @@ F_saveblock
 	ld c, e				; to write out.
 	jr .writeloop
 	ret
+
+;-----------------------------------------------------------------------
+; F_detectpages
+; Detects which 128K pages are in use. The .SNA format just supports the
+; ROMs from 0x7FFD and not the ports used by the +3/+2A exclusively. This
+; is unlikely to be a problem for the vast majority of programs.
+; Returns with carry set if pages could not be detected.
+F_detectpages
+
+	; Detect the paged in ROM.
+	ld hl, 0x8000			; Swap out a chunk of main RAM
+	ld de, SAVERAM			; into Spectranet memory so that
+	ld bc, 512			; the ROM can be examined
+	ldir
+	ld hl, F_romdetect		; Copy the ROM detection routine
+	ld de, 0x8000			; to main RAM.
+	ld bc, F_romdetect_sz
+	ldir
+	ld (MAINSPSAVE), sp		; save the stack pointer
+	ld sp, TEMPSP			; and put the stack in main RAM
+	call 0x8000			; Call the ROM detect routine
+	ld sp, (MAINSPSAVE)		; Restore the stack pointer
+	push af				; save flags
+	ld a, e				; get ROM flag
+	ld (SNA_7FFD), a
+	ld hl, SAVERAM			; Restore the memory chunk that
+	ld de, 0x8000			; we used.
+	ld bc, 512
+	ldir
+	pop af				; restore flags so carry is set
+	ret c
+
+	; Detect the RAM page in use
+	call F_detectram
+	ret			
+
+; THIS ROUTINE MUST BE COPIED TO RAM FIRST!
+; Returns the value of port 7FFD bit 4 in the E register
+; Returns the value of port 1FFD bit 2 in the D register
+F_romdetect
+	call PAGEOUT			; page out the Spectranet ROM
+	ld a, (0x0008)			; examine 0x0008 in the ROM
+	cp 0xFB				; ROM 0 for all models
+	jr z, .editor
+	cp 0xC3				; ROM 1 - Plus 3
+	jr z, .plusthreesyn
+	cp 0x50				; ROM 2 - Plus 3 DOS
+	jr z, .plusthreedos
+	cp 0x2A				; BASIC (all models)
+	jr nz, .unknown
+.basic
+	ld de, 0x0210			; Bit 2 of 1FFD, bit 4 of 7FFD
+	jr .exit
+.editor
+	ld de, 0x0000			; Neither 1FFD nor 7FFD set
+	jr .exit
+.plusthreedos
+	ld de, 0x0200			; only 0x1FFD set
+	jr .exit
+.plusthreesyn
+	ld de, 0x0010			; only 0x7FFD set
+.exit	
+	ld bc, CTRLREG
+	ld a, 1				; page in Spectranet ROM
+	out (c), a			
+	ret
+.unknown
+	scf
+	jr .exit
+
+F_romdetect_sz	equ $-F_romdetect
+
+;-------------------------------------------------------------------------
+; F_detectram: See what RAM page is paged in.
+F_detectram
+	; Next detect the RAM page. For this we need to copy a small
+	; chunk from 0xC000 in our memory, and then put a string into
+	; that RAM page. Then start flipping through RAM pages with
+	; port 0x7FFD.	
+	ld hl, 0xC000			; Preserve the stuff at 0xC000
+	ld de, SAVERAM
+	ld bc, SNSTRINGLEN
+	ldir
+	ld hl, SNSTRING			; and place our string there.
+	ld de, 0xC000
+	ld bc, SNSTRINGLEN
+	ldir
+
+	xor a				; start at page 0
+.detectloop
+	ld bc, 0x7FFD			; port 0x7FFD
+	out (c), a			; switch page
+.snapcmp
+	push af
+	call .compare
+	jr nz, .next
+.continue
+	pop de
+	ld a, (SNA_7FFD)		; get current flags
+	or d				; merge in the RAM page flags
+	ld (SNA_7FFD), a		; Save the page.
+	jr .restoreram
+.next
+	pop af
+	inc a				; go to the next page
+	cp 0x08				; Gone through every page?
+	jr nz, .detectloop
+	scf				; Oops - couldn't find the string!
+	ret
+
+.restoreram
+	; Check whether paging really was happening...
+	ld a, d				; page is in D
+	and a				; if it's not page zero
+	jr nz, .restoreram2		; then paging really happened
+	inc a				; if not increment the page and
+	and 0x07			; see if our string's still there
+	ld bc, 0x7FFD
+	out (c), a
+	call .compare
+	ld a, (SNA_7FFD)
+	ld bc, 0x7FFD			; restore RAM page if it
+	out (c), a			; actually changed
+	jr nz, .restoreram2		; and restore original RAM
+
+	scf				; 128K snapshot not possible
+.restoreram2
+	ld hl, SAVERAM			; Restore the original contents
+	ld de, 0xC000			; of RAM.
+	ld bc, SNSTRINGLEN
+	ldir
+	ret
+
+	; This routine looks for our string in RAM. Returns with Z
+	; set if the string was found.
+.compare
+	ld hl, 0xC000
+	ld de, SNSTRING
+	ld bc, SNSTRINGLEN
+.cploop
+	ld a, (de)
+	cpi
+	ret nz				; Not matched.
+	inc de
+	jp pe, .cploop
+	ret
+
+SNSTRING	defb "Spectranet snapshot"
+SNSTRINGLEN	equ $-SNSTRING	
 
