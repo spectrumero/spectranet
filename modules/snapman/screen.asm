b210722f2ea65485604d7e7fa10832ff9cdfe192
winston <dyls@alioth.net>
2009-12-03 22:32:17 +0000

    Selection box for snapshot manager

--- modules/snapman/screen.asm
+++ modules/snapman/screen.asm
@@ -21,28 +21,136 @@
 ;THE SOFTWARE.
 
 ;------------------------------------------------------------------------
+; F_makeselection
+; HL = start address of box
+; B = number of lines the box has
+; C = maximum column number (32 cols)
+; DE = address of first item to show
+F_makeselection
+	ld (v_selstart), hl	; save start address
+	ld (v_maxcolumn), bc	; and save the num lines + end column
+	push hl			; TODO: optimize
+	push bc
+	push de
+	call F_bytesperline	; initialize line length
+	call F_cleararea
+	pop de
+	pop bc
+	pop hl
+		
+	ld a, l			; calculate start column
+	and 0x1F		; by masking out top 3 bits
+	ld (v_startcolumn), a	; save the start column
+	ld b, a			; calculate number of 42-col characters
+	ld a, c			; (end column in A)
+	sub b			; number of 8 pixel columns
+	dec a
+	ld (v_barlen), a	; - this is needed later for the bar len
+	inc a
+	ld b, a			; save this
+	sra a			; divide by four
+	sra a
+	add a, b		; and add to the number of 8 px columns
+	inc a
+	ld (v_42colsperln),a	; then save.
+	ld a, l			; Initialize the position of the 42 col
+	and 0xE0		; print routine to the top of the box.
+	ld l, a
+	ld (v_row), hl		; Spectranet sysvar
+	xor a
+	ld (v_selrow), a	; reset row counter
+	ld (v_selecteditem), a	; reset selected item number
+
+	ex de, hl		; get first item to print
+	ld a, (v_sellines)	; number of lines we can fit
+	ld b, a
+.printloop
+	ld e, (hl)		; low order of string lookup
+	inc hl
+	ld d, (hl)		; high order of string lookup
+	inc hl
+	ld a, d
+	or e			; is it zero?
+	jr z, .doneprinting
+	ex de, hl		; move string's address to HL
+	push de			; save string pointer table address
+	push bc			; save counter
+	call F_cprint		; print the string
+	pop bc
+	pop de
+	ex de, hl
+	djnz .printloop
+.doneprinting
+
+	ld hl, (v_selstart)	; initialize the selection bar
+	call F_fbuf2attr	; find address of attr cell
+	ld (v_baraddr), hl
+	xor a			; initialize bar position
+	ld (v_barpos), a
+	call F_putbar
+	call F_inputloop
+	ret
+
+;------------------------------------------------------------------------
+; F_
+
+;------------------------------------------------------------------------
+; F_cprint
+; Constrained print routine. Print from the start column to the maximum
+; column or until NULL, whichever is earliest.
+; HL = pointer to string
+F_cprint
+	xor a
+	ld (v_rowcount), a	; prevent 42 col routine scroll
+	ld a, (v_startcolumn)	; get the 32 column position and
+	ld b, a			; convert it to a 42 column start
+	sra a
+	sra a
+	inc a
+	add a, b
+	ld (v_column), a	; set Spectranet 42 col print routine col
+	ld b, 0
+.loop
+	ld a, (hl)
+	and a
+	jr z, .cr
+	call PUTCHAR42
+	inc hl
+	inc b			; increment 'columns printed so far'
+	ld a, (v_42colsperln)
+	cp b			; reached the limit?
+	jr nz, .loop
+.cr
+	ld a, '\n'
+	call PUTCHAR42
+	ret
+
+;------------------------------------------------------------------------
+; F_bytesperline
+; Calculates bytes per line. HL = start address of the line.
+F_bytesperline
+	push bc
+	ld a, l
+	and 0x1F
+	ld b, a
+	ld a, (v_maxcolumn)
+	sub b
+	ld (v_movebytes), a	; store it as a 2 byte value
+	xor a			; so it can be loaded into BC
+	ld (v_movebytes+1), a	; in one go for LDIR.
+	pop bc
+	ret
+
+;------------------------------------------------------------------------
 ; F_scrollarea
 ; Scroll an area of the screen.
 ; HL = pointer to first line to scroll
 ; B = number of lines to scroll
 ; C = 0 = scroll normal way, nonzero scroll reverse
-F_scrollarea
+F_scrollforward
 	push bc			; save counter
-	ld a, l			; Get value of L to calculate bytes to move..
-	and 0x1F		; Mask away top few bits
-	ld b, a			; save it in B...
-	ld a, (v_maxcolumn)	
-	sub b			; and calculate value.
-	ld (v_movebytes), a
-	xor a
-	ld (v_movebytes+1), a
-	cp c			; forward/reverse?
-	jr nz, F_scrollreverse
-
-	ld a, l			; moving across thirds?
-	and 0xE0		; if so none of the top 3 bits are set
-	ld b, 8			; number of iterations
-	push hl
+	call .advance		; advance one line (need to scroll
+	push hl			; box size minus 1)
 	jr z, .moveloopthird
 .moveloop
 	push bc			; save line counter
@@ -64,26 +172,10 @@ F_scrollarea
 	ret z			; all lines scrolled
 
 	push af			; save counter
-	ld a, l
-	and 0xE0
-	cp 0xE0			; about to go into the next third?
-	jr z, .nextthird
-	ld a, 32
-	add a, l		; next line
-	ld l, a
+	call .advance
 	push hl
-	ld b, 8			; number of iterations per line
-	jr .moveloop
+	jr nz, .moveloop
 
-.nextthird
-	ld a, l
-	and 0x1F		; clear 3 most significant bits
-	ld l, a			; set LSB of pointer
-	ld a, h
-	add 8
-	ld h, a			; set MSB of pointer
-	push hl			; save HL
-	ld b, 8			; move 8 lines
 .moveloopthird
 	push bc
 	ld a, l
@@ -101,11 +193,31 @@ F_scrollarea
 	djnz .moveloopthird
 	jr .nextline
 
+.advance
+        ld a, l
+        and 0xE0
+        cp 0xE0                 ; about to go into the next third?
+        jr z, .nnextthird
+        ld a, 32
+        add a, l                ; next line
+        ld l, a
+        ld b, 8                 ; number of iterations per line
+	ret
+.nnextthird
+        ld a, l
+        and 0x1F                ; clear 3 most significant bits
+        ld l, a                 ; set LSB of pointer
+        ld a, h
+        add 8
+        ld h, a                 ; set MSB of pointer
+        ld b, 8                 ; move 8 lines
+	xor a
+	ret
+
 F_scrollreverse	
-	ld a, l			; starting with a 3rd boundary?
-	and 0xE0		; if so all of the top 3 bits are
-	cp 0xE0			; set
-	ld b, 8			; number of iterations
+	dec b
+	push bc
+	call .retreat		; up one line, scroll box size - 1
 	push hl
 	jr z, .nextthird
 .moveloop
@@ -128,25 +240,10 @@ F_scrollreverse
 	ret z			; and leave if we've finished all lines
 
 	push af			; store counter
-	ld a, l			; about to cross a third boundary?
-	and 0xE0
-	jr z, .nextthird
-	ld a, l
-	sub 32			; move up a line
-	ld l, a
+	call .retreat
 	push hl	
-	ld b, 8
-	jr .moveloop
+	jr nz, .moveloop
 
-.nextthird
-	ld a, l
-	or 0xE0			; set top 3 bits of the pointer
-	ld l, a			; set LSB of pointer
-	ld a, h
-	sub 8
-	ld h, a			; set MSB of pointer
-	push hl			; save HL
-	ld b, 8			; move 8 lines
 .moveloopthird
 	push bc
 	ld a, l
@@ -164,21 +261,32 @@ F_scrollreverse
 	djnz .moveloopthird
 	jr .nextline
 
+.retreat
+        ld a, l                 ; about to cross a third boundary?
+        and 0xE0
+        jr z, .nextthird
+        ld a, l
+        sub 32                  ; move up a line
+        ld l, a
+        ld b, 8
+	ret
+.nextthird
+        ld a, l
+        or 0xE0                 ; set top 3 bits of the pointer
+        ld l, a                 ; set LSB of pointer
+        ld a, h
+        sub 8
+        ld h, a                 ; set MSB of pointer
+        ld b, 8                 ; move 8 lines
+	xor a			; zero flag set
+	ret
+
 ;--------------------------------------------------------------------------
 ; F_cleararea: Clears a defined area.
 ; HL = Start address
 ; B = Number of lines to clear
 F_cleararea
 	push bc			; save counter
-	ld a, l			; Get value of L to calculate bytes to move..
-	and 0x1F		; Mask away top few bits
-	ld b, a			; save it in B...
-	ld a, (v_maxcolumn)	
-	sub b			; and calculate value
-	dec a			; minus 1
-	ld (v_movebytes), a
-	xor a
-	ld (v_movebytes+1), a
 	push hl
 	ld b, 8			; clear 8 scanlines
 .clearloop
@@ -188,6 +296,7 @@ F_cleararea
 	inc e
 	push hl
 	ld bc, (v_movebytes)
+	dec bc
 	xor a
 	ld (hl), a
 	ldir
@@ -199,7 +308,7 @@ F_cleararea
 	pop hl
 	pop af			; get counter
 	dec a
-	ret z			; all lines cleared
+	jr z, .savelastline	; all lines cleared
 
 	push af			; save counter
 	ld a, l
@@ -223,6 +332,32 @@ F_cleararea
 	push hl			; save HL
 	ld b, 8			; move 8 lines
 	jr .clearloop
+
+.savelastline
+	ld (v_selend), hl	; save the bottom row address
+	ret
+
+;------------------------------------------------------------------------
+; F_clearline
+; Clears a line. HL = start address.
+F_clearline
+	ld b, 8
+.clearloop
+	push bc
+	ld d, h
+	ld e, l
+	inc e
+	push hl
+	ld bc, (v_movebytes)
+	dec bc
+	xor a
+	ld (hl), a
+	ldir
+	pop hl
+	inc h
+	pop bc
+	djnz .clearloop
+	ret
 
 ;------------------------------------------------------------------------
 ; F_movebar
@@ -252,6 +387,7 @@ F_movebar
 	add hl, de
 .move
 	ld (v_baraddr), hl	; Save the new row pointer
+F_putbar_impl
 	ld d, h
 	ld e, l
 	inc e
@@ -261,4 +397,149 @@ F_movebar
 	ld (hl), SELECTED_ATTR	; Colour to draw
 	ldir
 	ret
+
+;------------------------------------------------------------------------
+; F_putbar
+; Draws the bar at the current start address.
+F_putbar
+	ld hl, (v_baraddr)
+	ld a, (v_barlen)
+	jp F_putbar_impl
+
+;------------------------------------------------------------------------
+; F_fbuf2attr
+; Convert pixel buffer addresses to attribute addresses
+F_fbuf2attr
+	ld a, l			; get LSB of frame buffer address
+	and 0xE0		; mask out bottom bits
+	rlca			; and move to lowest 3 bits
+	rlca
+	rlca
+	ld e, a			; store in low order of DE
+	ld a, h			; high order - 0x40, 0x48 or 0x50
+	sub 0x40		; now 0x00, 0x08 or 0x10
+	add a, e		; and add the current third-of-a-row
+	ld e, a			; and store in the low order of DE
+	ld d, 0			; zero the high order
+	ex de, hl
+	add hl, hl		; multiply by 32...2
+	add hl, hl		; 4
+	add hl, hl		; 8
+	add hl, hl		; 16
+	add hl, hl		; 32
+	ld a, e			; now derive the column
+	and 0x1F
+	add a, l		; add it to the offset that was calculated
+	ld l, a			; now HL = offset
+	ld de, ATTR_BASE
+	add hl, de		; and create the address
+	ret
+
+;------------------------------------------------------------------------
+; F_inputloop
+; Deal with keyboard input.
+F_inputloop
+.inputloop
+	call GETKEY		; get the actual key
+	push af			; save it
+	call KEYUP		; and wait for keyup
+	pop af
+	cp 0x0A			; KEY_DOWN
+	jr z, .bardown
+	cp 0x0B			; KEY_UP
+	jr z, .barup
+	ret
+.bardown
+	ld a, (v_sellines)	; hit the bottom?
+	dec a			; convert to 'index'
+	ld b, a
+	ld a, (v_barpos)
+	cp b
+	jr z, .scrolldown
+	inc a
+	ld (v_barpos), a	; update position
+	ld a, (v_selecteditem)	; increment the selected item index
+	inc a
+	ld (v_selecteditem), a
+	xor a
+	call F_movebar
+	jr .inputloop
+.barup
+	ld a, (v_barpos)
+	and a
+	jr z, .scrollup
+	dec a
+	ld (v_barpos), a
+	ld a, (v_selecteditem)	; decrement the selected item index
+	dec a
+	ld (v_selecteditem), a
+	inc a			; ensure A is nonzero
+	call F_movebar
+	jr .inputloop
+
+
+.scrolldown
+	ld a, (v_lastitemidx)	
+	ld b, a			; compare the last item index
+	ld a, (v_selecteditem)	; with the current
+	cp b
+	jr z, .inputloop	; already at the last item
+	ld a, (v_selecteditem)	; Increment the selected item index.
+	inc a
+	ld (v_selecteditem), a
+	rlca			; double it to calculate
+	ld d, 0			; the pointer table address
+	ld e, a		
+	ld hl, (v_stringtable)	; get the string table start address
+	add hl, de		; and calculate the current pointer
+	ld e, (hl)		; load the pointer into DE
+	inc hl
+	ld d, (hl)
+	push de			; save the pointer itself
+	ld hl, (v_selstart)	; scroll the box
+	ld a, (v_sellines)
+	ld b, a
+	call F_scrollforward
+
+	ld hl, (v_selend)	; get end address
+	call F_clearline	; clear the line in preparation for new text
+	ld hl, (v_selend)
+	ld a, l
+	and 0xE0		; mask out bottom bits
+	ld l, a
+	ld (v_row), hl		; set print routine to bottom line row
+	pop hl			; now get the string back
+	call F_cprint
+	jp .inputloop
+
+.scrollup
+	ld a, (v_selecteditem)	; check we've not hit zero
+	and a
+	jp z, .inputloop
+	dec a
+	ld (v_selecteditem), a	; update the selected item
+	ld hl, (v_stringtable)	; string table start address
+	rlca			; double A
+	ld d, 0
+	ld e, a
+	add hl, de		; calculate current top string pointer
+	ld e, (hl)
+	inc hl
+	ld d, (hl)
+	push de			; save the string pointer
+	ld hl, (v_selend)	; bottom line of the selection box
+	ld a, (v_sellines)
+	ld b, a
+	call F_scrollreverse	; scroll up a line
+	
+	ld hl, (v_selstart)	; get top line address
+	call F_clearline
+	ld hl, (v_selstart)	; and again to set the print routine
+	ld a, l
+	and 0xE0		; mask out bottom 5 bits
+	ld l, a
+	ld (v_row), hl		; set the print routine's row address
+	pop hl			; get the string pointer
+	call F_cprint
+	jp .inputloop
 
