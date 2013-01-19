d07784c1e3edc5ada239c9c38a5bcd09f6c305a8
winston <dyls@alioth.net>
2013-01-19 17:38:24 +0000

    ...and include stat.inc

--- modules/streams/fileio.asm
+++ modules/streams/fileio.asm
@@ -20,6 +20,7 @@
 ;OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ;THE SOFTWARE.
 .include	"fcntl.inc"
+.include	"stat.inc"
 .include	"spectranet.inc"
 .include	"defs.inc"
 .include	"sysvars.inc"
