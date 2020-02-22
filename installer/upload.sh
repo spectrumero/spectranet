15981819d4d5fb5312b07405daef8ef16d61ba42
Dylan Smith <winston@adun.alioth.net>
2020-02-22 10:58:48 +0000

    Remove references to svn in the build

--- installer/upload.sh
+++ installer/upload.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-svn up
-echo `svn info .|grep -m 1 Rev|cut -d " " -f 2` > version.txt
-scp *.tzx *.tap *.bin providence.alioth.net:/var/www/vhosts/spectrum/www/downloads
+cat ../rom/VERSION > version.txt
+scp *.tzx *.tap *.bin vexed4.alioth.net:/var/www/vhosts/spectrum/www/downloads
 scp *.bin vexed4.alioth.net:/home/tnfsd/tnfsroot/firmware
 scp version.txt vexed4.alioth.net:/home/tnfsd/tnfsroot/firmware
 
